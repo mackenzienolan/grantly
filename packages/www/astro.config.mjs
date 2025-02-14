ca2e466c4a61354161802e66f5341af2019357d5
Mackenzie Nolan <nolanmackenzie.a@gmail.com>
2025-02-13 22:02:24 -0500

    added features table + other updates

--- packages/www/astro.config.mjs
+++ packages/www/astro.config.mjs
@@ -2,12 +2,21 @@
 import react from "@astrojs/react";
 import clerk from "@clerk/astro";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 import aws from "astro-sst";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), clerk()],
+  integrations: [
+    react(),
+    clerk(),
+    icon({
+      include: {
+        lucide: ["*"], // (Default) Loads entire Material Design Icon set
+      },
+    }),
+  ],
   output: "server",
   adapter: aws(),
   vite: {
