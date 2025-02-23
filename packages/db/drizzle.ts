88f9196a2b39c09c0c125b0d78c490476ba13386
Mackenzie Nolan <nolanmackenzie.a@gmail.com>
2025-02-23 14:56:05 -0500

    updated postgres-v2

--- packages/db/drizzle.ts
+++ packages/db/drizzle.ts
@@ -4,12 +4,12 @@ import { Resource } from "sst";
 import * as schema from "./schema";
 
 const pool = new Pool({
-  ssl: Resource.App.stage === "main" ? { rejectUnauthorized: true } : false,
-  host: Resource.postgres.host,
-  port: Resource.postgres.port,
-  user: Resource.postgres.username,
-  password: Resource.postgres.password,
-  database: Resource.postgres.database,
+  ssl: Resource.App.stage === "prod" ? { rejectUnauthorized: true } : false,
+  host: Resource["postgres-v2"].host,
+  port: Resource["postgres-v2"].port,
+  user: Resource["postgres-v2"].username,
+  password: Resource["postgres-v2"].password,
+  database: Resource["postgres-v2"].database,
 });
 
 export const db = drizzle(pool, { schema });
