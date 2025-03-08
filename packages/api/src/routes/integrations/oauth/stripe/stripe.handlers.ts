7510b9c441776d8928dc7bf61a1c0a0f5738a6d2
Mackenzie Nolan <nolanmackenzie.a@gmail.com>
2025-03-08 18:38:34 -0500

    added migration + updated stripe handler

--- packages/api/src/routes/integrations/oauth/stripe/stripe.handlers.ts
+++ packages/api/src/routes/integrations/oauth/stripe/stripe.handlers.ts
@@ -1,22 +1,23 @@
-import type { AppRouteHandler } from '../../../../lib/types';
-
-import { db, integrationsTable } from '@grantly/db';
-import { getAuth } from '@hono/clerk-auth';
-import { DateTime } from 'luxon';
-import { Resource } from 'sst';
-import * as HttpStatusCodes from 'stoker/http-status-codes';
-import * as HttpStatusPhrases from 'stoker/http-status-phrases';
-import Stripe from 'stripe';
-import type { StripeOauthCreateRoute } from './stripe.routes';
+import { db, integrationsTable } from "@grantly/db";
+import events from "@grantly/events/core";
+import { getAuth } from "@hono/clerk-auth";
+import { DateTime } from "luxon";
+import { Resource } from "sst";
+import { bus } from "sst/aws/bus";
+import * as HttpStatusCodes from "stoker/http-status-codes";
+import * as HttpStatusPhrases from "stoker/http-status-phrases";
+import Stripe from "stripe";
+import type { AppRouteHandler } from "../../../../lib/types";
+import type { StripeOauthCreateRoute } from "./stripe.routes";
 
 const stripe = new Stripe(Resource.STRIPE_SECRET_KEY.value);
 
 export const stripeOauthCreate: AppRouteHandler<StripeOauthCreateRoute> = async (c) => {
-  const { code } = c.req.valid('query');
+  const { code } = c.req.valid("query");
   const auth = getAuth(c);
 
   const orgId = auth?.orgId;
-  const userId = Number(auth?.sessionClaims?.['external_id']);
+  const userId = Number(auth?.sessionClaims?.["external_id"]);
 
   if (!auth || !userId || !orgId) {
     return c.json({ message: HttpStatusPhrases.UNAUTHORIZED }, HttpStatusCodes.UNAUTHORIZED);
@@ -24,41 +25,43 @@ export const stripeOauthCreate: AppRouteHandler<StripeOauthCreateRoute> = async 
 
   try {
     const token = await stripe.oauth.token({
-      grant_type: 'authorization_code',
+      grant_type: "authorization_code",
       code,
     });
 
     if (!token.refresh_token) {
-      c.var.logger.error({ token }, 'No refresh token');
+      c.var.logger.error({ token }, "No refresh token");
       return c.json(
         {
-          message: 'No refresh token',
+          message: "No refresh token",
         },
         HttpStatusCodes.BAD_REQUEST
       );
     }
 
     const insert = {
-      type: 'stripe',
+      type: "stripe",
       accessToken: token.access_token,
       accessTokenExpiresAt: DateTime.now().plus({ hours: 1 }).toJSDate(),
       refreshToken: token.refresh_token,
       refreshTokenExpiresAt: DateTime.now().plus({ year: 1 }).toJSDate(),
       _raw: token,
-      status: 'active',
+      status: "active",
       teamId: orgId,
     } satisfies typeof integrationsTable.$inferInsert;
-    await db.insert(integrationsTable).values(insert);
+    const [integrationRes] = await db.insert(integrationsTable).values(insert).returning();
+
+    await bus.publish(Resource.bus, events["integration.created"], integrationRes);
 
     return c.json(
       {
-        message: 'Stripe OAuth created',
+        message: "Stripe OAuth created",
       },
       HttpStatusCodes.OK
     );
   } catch (err) {
     if (err instanceof Error) {
-      c.var.logger.error({ err: err.message }, 'Error creating stripe oauth');
+      c.var.logger.error({ err: err.message }, "Error creating stripe oauth");
       return c.json(
         {
           message: err.message,
@@ -69,7 +72,7 @@ export const stripeOauthCreate: AppRouteHandler<StripeOauthCreateRoute> = async 
     c.var.logger.error(err);
     return c.json(
       {
-        message: 'Internal server error',
+        message: "Internal server error",
       },
       HttpStatusCodes.INTERNAL_SERVER_ERROR
     );
