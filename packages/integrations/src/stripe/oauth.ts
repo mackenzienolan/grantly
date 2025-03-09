80321191ebadade841d9b0bc12b64f2a79c57c53
Mackenzie Nolan <nolanmackenzie.a@gmail.com>
2025-03-08 19:34:09 -0500

    Issue is coming from using different STRIPE_SECRET keys between sandboxes

--- packages/integrations/src/stripe/oauth.ts
+++ packages/integrations/src/stripe/oauth.ts
@@ -1,5 +1,4 @@
 import { integrationsTable } from "@grantly/db";
-import { DateTime } from "luxon";
 import { Resource } from "sst";
 
 import Stripe from "stripe";
@@ -16,14 +15,15 @@ export async function refreshStripeAccessToken(refreshToken: string, clientSecre
 }
 
 export async function refreshStripeIntegration(integration: typeof integrationsTable.$inferSelect) {
-  if (
-    DateTime.now().minus({ minutes: 1 }) > DateTime.fromJSDate(integration.accessTokenExpiresAt)
-  ) {
-    return {
-      access_token: integration.accessToken,
-    } satisfies Stripe.OAuthToken;
-  }
+  // if (
+  //   DateTime.now().minus({ minutes: 1 }) > DateTime.fromJSDate(integration.accessTokenExpiresAt)
+  // ) {
+  //   return {
+  //     access_token: integration.accessToken,
+  //   } satisfies Stripe.OAuthToken;
+  // }
 
+  console.log("refreshing stripe integration", integration.refreshToken);
   const tokens = await refreshStripeAccessToken(
     integration.refreshToken,
     Resource.STRIPE_SECRET_KEY.value
